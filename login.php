27f306ff2a16d5d30000642aebd93eb0f6882bc9
duuyguu <133018682+kontees@users.noreply.github.com>
2023-05-17 15:26:54 +0300

    güncelleme
    apı kısmıı form verilerini görüntüleme kısmı gibi etkenleri halletim

--- login.php
+++ login.php
@@ -1,25 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login PHP</title>
+</head>
+<body>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $users = array(
-        array("email" => "g221210082@sakarya.edu.tr", "password" => "g221210082"),
-    );
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+// Kullanıcı adı ve şifreyi alın
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-    $loggedIn = false;
-    foreach ($users as $user) {
-        if ($email === $user["email"] && $password === $user["password"]) {
-            $loggedIn = true;
-            break;
-        }
-    }
+// Kullanıcı adı ve şifreyi kontrol et
+if ($email === 'g221210082' && $password === 'g221210082') {
+  // Doğru ise kullanıcı bilgilerini sakla
+  session_start();
+  $_SESSION['email'] = $email;
 
-    if ($loggedIn) {
-        header("Location: http://localhost/odev/duygu.html");
-        echo "Hoşgeldiniz " . $email . "! Giriş Başarılı.";
-    } else {
-        header("Location: http://localhost/odev/log%c4%b1n.html");
-        exit();
-    }
+  // Yönlendirme yapmadan önce birkaç saniye bekleyebilirsiniz
+   echo "Hoşgeldin, " . $email . "! Yönlendiriliyorsunuz...";
+   header("refresh:3;url=duygu.html");
+
+  // Yönlendirme yap
+  header("Location: duygu.html");
+  exit();
+} else {
+  echo "Kullanıcı adı veya şifre hatalı.";
 }
 ?>
+
+
+
+</body>
+</html>
+
+
